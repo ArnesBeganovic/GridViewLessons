46c9070bdd989ccdd9655d27ff1f1bba46fd6a05
ArnesBeganovic <arnesbeganovic@gmail.com>
2017-04-06 15:53:14 +0200

    Object DataSource Insert Delete Update

---
+++ GridViewUsefulStuff/Object Data Source/EmployeeDataAccessLayer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GridViewUsefulStuff
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+
+    public class EmployeeDataAccessLayer
+    {
+        public static List<Employee> GetAllEmployees()
+        {
+            /*
+             * SELECT METHOD FROM DATABASE
+             */
+            List<Employee> listEmployees = new List<Employee>();
+            string CS = ConfigurationManager.ConnectionStrings["QuotationDB"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("select * from EmployeeASPNETTutorial", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.City = rdr["Cty"].ToString();
+
+                    listEmployees.Add(employee);
+                }
+            }
+            return listEmployees;
+        }
+        public static void DeleteEmployee(int EmployeeId)
+        {
+            /*
+             * UPDATE SQL
+             */
+            string CS = ConfigurationManager.ConnectionStrings["QuotationDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand
+                    ("Delete from EmployeeASPNETTutorial where EmployeeId = @EmployeeId",con);
+                SqlParameter param = new SqlParameter("@EmployeeId", EmployeeId);
+                cmd.Parameters.Add(param);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public static int UpdateEmployee(int EmployeeId, string Name, string Gender, string City)
+        {
+            /*
+             *UPDATE SQL 
+             */
+            string CS = ConfigurationManager.ConnectionStrings["QuotationDB"].ConnectionString;
+            using(SqlConnection con = new SqlConnection(CS))
+            {
+                string updateQuarry = "update EmployeeASPNETTutorial set Name = @Name, " +
+                    "Gender = @Gender, Cty = @City where EmployeeId = @EmployeeId";
+                SqlCommand cmd = new SqlCommand(updateQuarry,con);
+                SqlParameter paramOriginalEmployeeId = new SqlParameter("@EmployeeId", EmployeeId);
+                cmd.Parameters.Add(paramOriginalEmployeeId);
+                SqlParameter paramOriginalNameId = new SqlParameter("@Name", Name);
+                cmd.Parameters.Add(paramOriginalNameId);
+                SqlParameter paramOriginalGenderId = new SqlParameter("@Gender", Gender);
+                cmd.Parameters.Add(paramOriginalGenderId);
+                SqlParameter paramOriginalCityId = new SqlParameter("@City", City);
+                cmd.Parameters.Add(paramOriginalCityId);
+                con.Open();
+                return cmd.ExecuteNonQuery();
+            }
+        }
+        public static int InsertEmployee(string Name, string Gender,string City)
+        {
+            /*
+             * INSERT SQL
+             */
+            string CS = ConfigurationManager.ConnectionStrings["QuotationDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                string insertQuery = "insert into EmployeeASPNETTutorial (Name, Gender, Cty) values(@Name,@Gender,@City)";
+                SqlCommand cmd = new SqlCommand(insertQuery,con);
+                SqlParameter nameParam = new SqlParameter("@Name", Name);
+                cmd.Parameters.Add(nameParam);
+                SqlParameter genderParam = new SqlParameter("@Gender", Gender);
+                cmd.Parameters.Add(genderParam);
+                SqlParameter cityParam = new SqlParameter("@City", City);
+                cmd.Parameters.Add(cityParam);
+                con.Open();
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ GridViewUsefulStuff/Object Data Source/ObjectDataSourceInsertEditDelete.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GridViewUsefulStuff.Object_Data_Source
+{
+    public partial class ObjectDataSourceInsertEditDelete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Lnb_Insert_Click(object sender, EventArgs e)
+        {
+            //Prepare parameters for SQL
+            ObjectDataSource1.InsertParameters["Name"].DefaultValue = ((TextBox)GridView.FooterRow.FindControl("tb_InsertName")).Text;
+            ObjectDataSource1.InsertParameters["Gender"].DefaultValue = ((DropDownList)GridView.FooterRow.FindControl("ddl_InsertGender")).SelectedValue;
+            ObjectDataSource1.InsertParameters["City"].DefaultValue = ((TextBox)GridView.FooterRow.FindControl("tb_InsertCity")).Text;
+
+            //U Html-u ima InsertCommand koja ce biti pozvana sa kodom ispod a kod iznad je vec napunio parametre
+            ObjectDataSource1.Insert();
+        }
+    }
+}
